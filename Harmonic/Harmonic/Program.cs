035585d6beb1a3f6a8fc6098581de0d8cfdaebe4
ovi2410 <guneshwarifegade@gmail.com>
2022-05-28 10:09:27 +0530

    [ovi]harmonic no

---
+++ Harmonic/Harmonic/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace HarmonicNo
+{
+    class Program
+    {
+        static double NthHarmonic(int N)
+        {
+            float harmonic = 1;
+            int i;
+            for(i = 2;i <=N;i++)
+            {
+                harmonic += (float)1 / i;
+            }
+            return harmonic;
+        }
+        //driver code
+        static public void Main()
+        {
+            int N = 8;
+            Console.WriteLine(NthHarmonic (N));
+        }
+    }
+}
